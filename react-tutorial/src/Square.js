80a4b9052e54cda676f7bf5234d990334a5ef1a5
ysera34 <ycyoon34@gmail.com>
2017-12-11 15:51:37 +0900

    react tutorial step1: overview

---
+++ react-tutorial/src/Square.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value : null,
+        };
+    }
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+export default Square;

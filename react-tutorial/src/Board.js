06832ace71afef8a5691f04cda460a0e35919e63
ysera34 <ycyoon34@gmail.com>
2017-12-11 19:10:30 +0900

    react tutorial step3: storing a history

--- react-tutorial/src/Board.js
+++ react-tutorial/src/Board.js
@@ -10,59 +10,18 @@ class Board extends Component {
         }
     }
 
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[b] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        })
-    }
-
     renderSquare(i) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const winner = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
